6498515e6f15c33760e4a3af5c43507d7f68bcb0
Erick Lui <erickjin.lui@gmail.com>
2018-03-29 16:52:42 -0700

    added styles folder in public folder

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Table } from './table';
-import ReactPaginate from 'react-paginate';
 import players from '../players.json';
 
 export default class App extends Component {
@@ -19,17 +18,6 @@ export default class App extends Component {
     return (
       <div>
         <Table players={players.slice(0,5)}/>
-        <ReactPaginate
-          previousLabel={'previous'}
-          nextLabel={'next'}
-          breakLabel={<a href=''>...</a>}
-          pageCount={10}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={this.handlePageClick}
-          containerClassName={'pagination'}
-          subContainerClassName={'pages pagination'}
-          activeClassName={'active'} />
       </div>
     );
   }
