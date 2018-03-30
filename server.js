fc23d50ecb6b75d0dbdccbc5e480e6cbf7f7c948
Erick Lui <erickjin.lui@gmail.com>
2018-03-29 19:50:53 -0700

    updated pagination to navigate up to displayedTable instead of players array

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('test server');
 });
 
 app.listen(port, () => console.log(`running on port ${port}`));
